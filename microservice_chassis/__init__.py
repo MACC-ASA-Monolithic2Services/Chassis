ebf4308fac2ee428ac51ce16e757d99bea224a53
Telmo Rubio <telmo.rubio@alumni.mondragon.edu>
2025-11-16 22:11:07 +0100

    feat: added config to __init__

--- microservice_chassis/__init__.py
+++ microservice_chassis/__init__.py
@@ -1,6 +1,7 @@
 """Public exports for the microservice_chassis package."""
 
+from .config import Settings, settings
 from .dependencies import verify_jwt_token
 from .rabbitmq_logger import RabbitMQLogPublisher
 
-__all__ = ["verify_jwt_token", "RabbitMQLogPublisher"]
+__all__ = ["Settings", "settings", "verify_jwt_token", "RabbitMQLogPublisher"]
