8d23697ae837483b9c43ec74d19683a919bc7c34
Telmo Rubio <telmo.rubio@alumni.mondragon.edu>
2025-11-16 22:04:46 +0100

    feat: fixed __init__

--- microservice_chassis/__init__.py
+++ microservice_chassis/__init__.py
@@ -1,2 +1,6 @@
-from .dependencies import *
-from .rabbitmq_logger import *
+"""Public exports for the microservice_chassis package."""
+
+from .dependencies import verify_jwt_token
+from .rabbitmq_logger import RabbitMQLogPublisher
+
+__all__ = ["verify_jwt_token", "RabbitMQLogPublisher"]
