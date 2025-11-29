f7a1e99ff388474d6956e8e54e2a048dd11de0d3
Telmo Rubio <telmo.rubio@alumni.mondragon.edu>
2025-11-29 12:40:40 +0100

    feat: added consul

--- microservice_chassis/__init__.py
+++ microservice_chassis/__init__.py
@@ -1,8 +1,29 @@
 """Public exports for the microservice_chassis package."""
 
 from .config import Settings, settings
+from .consul_utils import (
+    deregister_consul_service,
+    get_consul_key_value_item,
+    get_consul_service,
+    get_consul_service_catalog,
+    get_consul_service_replicas,
+    register_consul_service,
+)
 from .dependencies import verify_jwt_token
-from .rabbitmq_logger import RabbitMQLogPublisher
 from .health import HealthConfig, create_health_router
+from .rabbitmq_logger import RabbitMQLogPublisher
 
-__all__ = ["Settings", "settings", "verify_jwt_token", "RabbitMQLogPublisher", "HealthConfig", "create_health_router"]
+__all__ = [
+    "Settings",
+    "settings",
+    "verify_jwt_token",
+    "RabbitMQLogPublisher",
+    "HealthConfig",
+    "create_health_router",
+    "register_consul_service",
+    "deregister_consul_service",
+    "get_consul_service",
+    "get_consul_key_value_item",
+    "get_consul_service_catalog",
+    "get_consul_service_replicas",
+]
